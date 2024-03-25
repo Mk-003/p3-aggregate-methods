41248a5270e6b9ebfe3f6992ee487d6a0d850ec8
joseph <michealkinuthia.m.k@gmail.com>
2024-03-25 12:22:07 +0300

    done

--- lib/enrollment.py
+++ lib/enrollment.py
@@ -29,6 +29,9 @@ class Course:
 
     def get_enrollments(self):
         return self._enrollments.copy()
+    
+    def course_count(self):
+     return len(self._enrollments)
 
 
 class Enrollment:
@@ -45,3 +48,11 @@ class Enrollment:
 
     def get_enrollment_date(self):
         return self._enrollment_date
+    
+    @classmethod
+ def aggregate_enrollments_per_day(cls):
+    enrollment_count = {}
+    for enrollment in cls.all:
+        date = enrollment.get_enrollment_date().date()
+        enrollment_count[date] = enrollment_count.get(date, 0) + 1
+    return enrollment_count
